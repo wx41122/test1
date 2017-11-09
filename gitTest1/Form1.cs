bd60961e73ee654247b7d7ac37376869c3341726
ChenCJ[陈超俊] <chencj@chinafastprint.com>
2017-11-09 14:52:59 +0800

    添加项目文件。

---
+++ gitTest1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gitTest1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private DataTable ConnectData(string strsql)
+        {
+            DataSet temp = new DataSet();
+            string strconnect = "Data Source=192.168.224.53;Integrated Security=false;Initial Catalog=FP_EMS_DB;User ID=sa;Password=nianhuahuayang";
+            System.Data.SqlClient.SqlDataAdapter sa = new System.Data.SqlClient.SqlDataAdapter(strsql, strconnect);
+            temp.Reset();
+
+            sa.Fill(temp);
+
+            if (temp.Tables.Count > 0)
+            { return temp.Tables[0]; }
+
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ gitTest1/Form2.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace gitTest1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.advPropertyGrid1 = new DevComponents.DotNetBar.AdvPropertyGrid();
+            this.vScrollBarAdv1 = new DevComponents.DotNetBar.VScrollBarAdv();
+            this.symbolBox1 = new DevComponents.DotNetBar.Controls.SymbolBox();
+            this.vScrollBarAdv2 = new DevComponents.DotNetBar.VScrollBarAdv();
+            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.textBoxX2 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.textBoxX3 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.textBoxX4 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            ((System.ComponentModel.ISupportInitialize)(this.advPropertyGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // advPropertyGrid1
+            // 
+            this.advPropertyGrid1.GridLinesColor = System.Drawing.Color.WhiteSmoke;
+            this.advPropertyGrid1.Location = new System.Drawing.Point(72, 111);
+            this.advPropertyGrid1.Name = "advPropertyGrid1";
+            this.advPropertyGrid1.Size = new System.Drawing.Size(75, 23);
+            this.advPropertyGrid1.TabIndex = 0;
+            this.advPropertyGrid1.Text = "advPropertyGrid1";
+            // 
+            // vScrollBarAdv1
+            // 
+            this.vScrollBarAdv1.Location = new System.Drawing.Point(147, 145);
+            this.vScrollBarAdv1.Name = "vScrollBarAdv1";
+            this.vScrollBarAdv1.Size = new System.Drawing.Size(75, 23);
+            this.vScrollBarAdv1.TabIndex = 1;
+            // 
+            // symbolBox1
+            // 
+            // 
+            // 
+            // 
+            this.symbolBox1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.symbolBox1.Location = new System.Drawing.Point(176, 342);
+            this.symbolBox1.Name = "symbolBox1";
+            this.symbolBox1.Size = new System.Drawing.Size(75, 23);
+            this.symbolBox1.TabIndex = 2;
+            this.symbolBox1.Text = "symbolBox1";
+            // 
+            // vScrollBarAdv2
+            // 
+            this.vScrollBarAdv2.Location = new System.Drawing.Point(212, 430);
+            this.vScrollBarAdv2.Name = "vScrollBarAdv2";
+            this.vScrollBarAdv2.Size = new System.Drawing.Size(75, 23);
+            this.vScrollBarAdv2.TabIndex = 3;
+            // 
+            // textBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX1.Location = new System.Drawing.Point(0, 0);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(100, 21);
+            this.textBoxX1.TabIndex = 0;
+            // 
+            // textBoxX2
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX2.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX2.Location = new System.Drawing.Point(0, 0);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(100, 21);
+            this.textBoxX2.TabIndex = 0;
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(144, 271);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 4;
+            this.buttonX1.Text = "buttonX1";
+            // 
+            // textBoxX3
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX3.Border.Class = "TextBoxBorder";
+            this.textBoxX3.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX3.Location = new System.Drawing.Point(488, 258);
+            this.textBoxX3.Name = "textBoxX3";
+            this.textBoxX3.PreventEnterBeep = true;
+            this.textBoxX3.Size = new System.Drawing.Size(100, 21);
+            this.textBoxX3.TabIndex = 5;
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(211, 179);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(75, 23);
+            this.buttonX2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX2.TabIndex = 6;
+            this.buttonX2.Text = "buttonX2";
+            // 
+            // textBoxX4
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX4.Border.Class = "TextBoxBorder";
+            this.textBoxX4.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX4.Location = new System.Drawing.Point(537, 312);
+            this.textBoxX4.Name = "textBoxX4";
+            this.textBoxX4.PreventEnterBeep = true;
+            this.textBoxX4.Size = new System.Drawing.Size(100, 21);
+            this.textBoxX4.TabIndex = 7;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 534);
+            this.Controls.Add(this.textBoxX4);
+            this.Controls.Add(this.buttonX2);
+            this.Controls.Add(this.textBoxX3);
+            this.Controls.Add(this.buttonX1);
+            this.Controls.Add(this.vScrollBarAdv2);
+            this.Controls.Add(this.symbolBox1);
+            this.Controls.Add(this.vScrollBarAdv1);
+            this.Controls.Add(this.advPropertyGrid1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.advPropertyGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevComponents.DotNetBar.AdvPropertyGrid advPropertyGrid1;
+        private DevComponents.DotNetBar.VScrollBarAdv vScrollBarAdv1;
+        private DevComponents.DotNetBar.Controls.SymbolBox symbolBox1;
+        private DevComponents.DotNetBar.VScrollBarAdv vScrollBarAdv2;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX2;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX3;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX4;
+    }
+}
